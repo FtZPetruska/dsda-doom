5f0182998b91ebbd8199c9a37540c4f591e34db4
Pierre Wendling <pierre.wendling.4@gmail.com>
2025-06-26 21:35:16 +0200

    midifile.c: Fix printf format warning.
    ntohl always returns a uint32_t, using inttypes.h' PRI format constants
    ensures we get the right one for the platform.

--- prboom2/src/MUSIC/midifile.c
+++ prboom2/src/MUSIC/midifile.c
@@ -23,6 +23,7 @@
 //
 //-----------------------------------------------------------------------------
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -604,7 +605,7 @@ static dboolean ReadFileHeader(midi_file_t *file, midimem_t *mf)
      || ntohl(file->header.chunk_header.chunk_size) != 6)
     {
         lprintf (LO_WARN, "ReadFileHeader: Invalid MIDI chunk header! "
-                        "chunk_size=%ld\n",
+                        "chunk_size=%" PRIu32 "\n",
                         ntohl(file->header.chunk_header.chunk_size));
         return false;
     }
